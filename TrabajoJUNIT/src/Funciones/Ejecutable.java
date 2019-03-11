0e2a492cf75ddbcfd0db11c87e414e3a8a4cc720
34654 <34654@DESKTOP-U56NI6J>
2019-03-11 18:01:29 +0100

    11/03/19

--- TrabajoJUNIT/src/Funciones/Ejecutable.java
+++ TrabajoJUNIT/src/Funciones/Ejecutable.java
@@ -6,8 +6,10 @@ public class Ejecutable {
 		// TODO Auto-generated method stub
 		System.out.println(FuncionesJUNIT.NumDiv(9));
 		System.out.println(FuncionesJUNIT.Intervalo(150));
-		System.out.println(FuncionesJUNIT.NumDivIntervalo(4,150));
-
+		System.out.println(FuncionesJUNIT.NumDivIntervalo(4, 150));
+		System.out.println(FuncionesJUNIT.CadenasMayusVocal("Andres", "Ricardo", "Dani"));
+		System.out.println(FuncionesJUNIT.NumeroCadena("pr0gramac10n"));
+		
+		
 	}
-
 }
--- TrabajoJUNIT/src/Funciones/FuncionesJUNIT.java
+++ TrabajoJUNIT/src/Funciones/FuncionesJUNIT.java
@@ -29,14 +29,28 @@ public class FuncionesJUNIT {
 	}
 	public static String CadenasMayusVocal(String cadena1, String cadena2,String cadena3) {
 		String result = null;
+		String a = "";
+		char pos;
 		result = cadena1+cadena2+cadena3;
 		for(int i = 0;i<result.length();i++) {
-			if((result.charAt(i)>64 && result.charAt(i)<91) || (result.charAt(i) == 'a'||'e'||'i'||'o'||'u')) {
-				
+			pos = result.charAt(i);
+			if(!(pos>64 && pos<91) && !(pos == 'a'||pos =='e'||
+					pos =='i'||pos =='o'||pos =='u')) {
+				a = a +pos;
 			}
 		}
-		
-		return result;
+		return a;
 	}
-
+	
+	public static int NumeroCadena(String cadena) {
+		int num = 0;
+		for(int i = 0; i < cadena.length();i++) {
+			if(cadena.charAt(i)>=48 && cadena.charAt(i)<=57) {
+				num += Integer.parseInt(cadena);
+				System.out.println(num);
+			}
+		}
+		return num;
+	}
+	
 }
