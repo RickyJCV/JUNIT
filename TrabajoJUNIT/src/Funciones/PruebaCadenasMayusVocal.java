17667a379dfc38343e3d509b368e5321df0937f6
Ricardo <a.ricardocabrera@gmail.com>
2019-03-20 16:43:05 +0100

    Pruebas realizadas a funcion CadenasMayusVocal

--- TrabajoJUNIT/src/Funciones/PruebaCadenasMayusVocal.java
+++ TrabajoJUNIT/src/Funciones/PruebaCadenasMayusVocal.java
@@ -12,7 +12,7 @@ class PruebaCadenasMayusVocal {
 
 	static int cont = 0;
 	static FuncionesJUNIT o = null;
-	
+
 	@BeforeAll
 	public static void crearObjeto() {
 		o = new FuncionesJUNIT();
@@ -35,35 +35,40 @@ class PruebaCadenasMayusVocal {
 		assertEquals("crdndrsnl", o.CadenasMayusVocal("Ricardo", "Andres", "Daniel"));
 
 	}
+
 	@DisplayName("Prueba de flujo de datos 2")
 	@Test
 	void resultadoEsperado2() {
 		assertEquals("crdndrsnl", o.CadenasMayusVocal("RicardO", "AndrEs", "DaniEl"));
 
 	}
+
 	@DisplayName("Prueba de flujo de datos 3")
 	@Test
 	void resultadoEsperado3() {
 		assertEquals("", o.CadenasMayusVocal("RICARDO", "ANDRES", "DANIEL"));
 
 	}
+
 	@DisplayName("Prueba de flujo de datos 4")
 	@Test
 	void resultadoEsperado4() {
 		assertEquals("1cr0nr3s4n13l", o.CadenasMayusVocal("R1carD0", "AnDr3s", "D4n13l"));
 
 	}
+
 	@DisplayName("Prueba de flujo de datos 5")
 	@Test
 	void resultadoEsperado5() {
 		assertEquals("ríndréssé", o.CadenasMayusVocal("María", "Andrés", "José"));
 
 	}
+
 	@DisplayName("Prueba de flujo de datos 6")
 	@Test
 	void resultadoEsperado6() {
 		assertEquals("n-4rí0sé/4nr3sly&4n4ls", o.CadenasMayusVocal("Ana-M4ría", "J0sé/4nDr3s", "Wily&C4n4les"));
 
 	}
-	
+
 }
